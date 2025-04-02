d66b12f493d2cb8115265367c0304096ef12f5e4
Krzychulec21 <krzychu123g@gmail.com>
2025-04-02 19:53:46 +0200

    init

---
+++ gh-repo-analyzer/src/main/java/com/githubtracker/gh_repo_analyzer/GhRepoAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.githubtracker.gh_repo_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhRepoAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhRepoAnalyzerApplication.class, args);
+	}
+
+}
---
+++ gh-repo-analyzer/src/test/java/com/githubtracker/gh_repo_analyzer/GhRepoAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubtracker.gh_repo_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhRepoAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
